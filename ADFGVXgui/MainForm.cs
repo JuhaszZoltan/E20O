40a09666760769804b643aee5619ec88c5c3d558
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-02-14 10:12:09 +0100

    matrix

--- ADFGVXgui/MainForm.cs
+++ ADFGVXgui/MainForm.cs
@@ -1,8 +1,11 @@
+using System.Text;
+
 namespace ADFGVXgui
 {
     public partial class MainForm : Form
     {
-        
+        public char[,] Kodtabla { get; set; }
+
         public MainForm()
         {
             InitializeComponent();
@@ -11,11 +14,28 @@ namespace ADFGVXgui
 
         private void OnLoadButtonClick(object? sender, EventArgs e)
         {
-            OpenFileDialog odf = new()
+            OpenFileDialog ofd = new()
             {
                 InitialDirectory = Application.StartupPath.Split("\\bin")[0] + "\\SRC\\",
             };
-            odf.ShowDialog();
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                Kodtabla = new char[6, 6];
+                using StreamReader sr = new(ofd.FileName, Encoding.UTF8);
+                int si = 0;
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine()!;
+                    for (int oi = 0; oi < sor.Length; oi++)
+                        Kodtabla[si, oi] = sor[oi];
+                }
+                KodtablaBetoltese();
+            }
+        }
+
+        private void KodtablaBetoltese()
+        {
+            throw new NotImplementedException();
         }
     }
 }
